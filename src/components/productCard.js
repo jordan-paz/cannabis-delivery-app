a655a317c749670a6c4f5a167b59e70b1d94f458
Jordan Paz <jordandavidpaz@gmail.com>
2019-07-27 21:05:47 -0700

    Fix styles in productCard and catalog

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,35 +1,28 @@
 import React, { useEffect, useState } from "react"
 import styled from "styled-components"
-import weed from "../images/weed-background.jpg"
+import weed from "../images/can.jpg"
 
 const ProductCardWrapper = styled.div`
-  border: solid 1px #000;
-  margin: 10px;
-  display: inline-grid;
-  grid-template-rows: 200px 70px;
+  border: solid 1px black;
 `
 
 const ProductCardImg = styled.img`
-  max-height: 200px;
-  width: 100%;
+  max-width: 100%;
 `
 
-const ProductCardTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-around;
-  margin-left: 10px;
-  padding: 20px 0;
+const ProductCardText = styled.div`
+  padding: 2px 16px;
 `
 
 const ProductCard = () => (
   <ProductCardWrapper>
     <ProductCardImg src={weed} />
-    <ProductCardTextWrapper>
-      <h3>Weed</h3>
+    <ProductCardText>
+      <h3>
+        <b>Weed</b>
+      </h3>
       <p>$25.00</p>
-    </ProductCardTextWrapper>
+    </ProductCardText>
   </ProductCardWrapper>
 )
 
