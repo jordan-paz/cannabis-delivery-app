edce9af8438234fc043d9209c365b182d3ee662a
Jordan Paz <jordandavidpaz@gmail.com>
2019-09-15 22:56:16 -0700

    Add authContext to login form to invoke api login on submit

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -14,6 +14,7 @@ const AuthProvider = ({ children }) => {
 
   const login = (email, password) => {
     api.login(email, password).then(data => {
+      console.log(data)
       setLocalStorage(data)
       setLoggedIn(true)
     })
