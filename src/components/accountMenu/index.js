a26b669325d09e67e0f911e50134c5b8e1eb16a0
Jordan Paz <jordandavidpaz@gmail.com>
2019-09-16 11:09:42 -0700

    Add drop down menu for account

--- src/components/accountMenu/index.js
+++ src/components/accountMenu/index.js
@@ -1,10 +1,30 @@
 import React from "react"
-import accountIcon from "../../images/account-icon.svg"
+import Menu from "./menu"
+import Button from "@material-ui/core/Button"
 
-export default () => {
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const firstName = localStorage.name.split(" ")[0]
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget)
+  }
+
+  function handleClose() {
+    setAnchorEl(null)
+  }
+
   return (
-    <span>
-      <img src={accountIcon} />
-    </span>
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Hi {firstName} &#9660;
+      </Button>
+      <Menu anchorEl={anchorEl} handleClose={handleClose} />
+    </div>
   )
 }
